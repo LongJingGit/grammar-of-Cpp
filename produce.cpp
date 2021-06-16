810173eaedbbebea8db943092337e437516d85db
JINGLONGGIT <jinglong_email@163.com>
2021-06-16 21:08:57 +0800

    other# update

--- produce.cpp
+++ produce.cpp
@@ -86,6 +86,27 @@ private:
 
 class ProduceOnlineBus
 {
+public:
+    ProduceOnlineBus(uint64_t productNum)
+        : productNum_(productNum),
+          trucked(0),
+          milkProded(0),
+          fjjProded(0),
+          cheeseProded(0)
+    {
+        milkCount_ = milkMultiple_ * productNum_;
+        fjjCount_ = fjjMultiple_ * productNum_;
+        needTruckNum_ = productNum_ / truckCapacity_;
+        milkNodeBlockingDeque = new SyncQueue<int>(milkCount_);
+        fjjNodeBlockingDeque = new SyncQueue<int>(fjjCount_);
+        cheeseNodeBlockingDeque = new SyncQueue<int>(chessCapacity_);
+    }
+    ~ProduceOnlineBus()
+    {
+    }
+
+    void start();
+
 private:
     uint64_t productNum_;           // 生产奶酪的数量
     uint64_t fjjMultiple_ = 1;      // 生产每块奶酪需要的发酵剂的数量
@@ -93,146 +114,95 @@ private:
     uint64_t chessCapacity_ = 1000; // 奶酪仓库容量
     uint64_t truckCapacity_ = 100;  // 单次运输的奶酪数量
     uint64_t needTruckNum_;         // 总共需要运送的次数
+    uint32_t milkCount_;            // 需要牛奶的总数量
+    uint32_t fjjCount_;             // 需要的发酵剂总数量
 
-    std::atomic<int> trucked(0); // 实际运送的次数
+    std::atomic<int> trucked; // 实际运送的次数
 
     /**
      * 各生产线生产次数
      **/
-    std::atomic<int> milkProded(0);   // 牛奶供应生产线生产数量
-    std::atomic<int> fjjProded(0);    // 发酵剂制作生产线生产数量
-    std::atomic<int> cheeseProded(0); // 奶酪生产线生产数量
+    std::atomic<int> milkProded;   // 牛奶供应生产线生产数量
+    std::atomic<int> fjjProded;    // 发酵剂制作生产线生产数量
+    std::atomic<int> cheeseProded; // 奶酪生产线生产数量
 
-    // SyncQueue<int> milkNodeBlockingDeque(milkMultiple_);    // 保存牛奶的队列
-    // SyncQueue<int> fjjNodeBlockingDeque(fjjMultiple_);      // 保存发酵剂的队列
-    // SyncQueue<int> cheeseNodeBlockingDeque(chessCapacity_); // 保存奶酪的队列
-
-    int milkCount = milkMultiple_ * productNum_;
-    int fjjCount = fjjMultiple_ * productNum_;
-    SyncQueue<MilkNode> milkNodeBlockingDeque(milkCount);         // 保存牛奶的队列
-    SyncQueue<FjjNode> fjjNodeBlockingDeque(fjjCount);            // 保存发酵剂的队列
-    SyncQueue<ChessNode> cheeseNodeBlockingDeque(chessCapacity_); // 保存奶酪的队列
-
-private:
-    // 牛奶
-    class MilkNode
-    {
-    public:
-        MilkNode(int seq)
-        {
-            std::cout << "生产牛奶：" << seq << std::endl;
-        }
-        ~MilkNode() {}
-    };
-
-    // 发酵剂
-    class FjjNode
-    {
-    public:
-        FjjNode(int seq)
-        {
-            std::cout << "生产发酵剂：" << seq << std::endl;
-        }
-        ~FjjNode() {}
-    };
-
-    // 奶酪
-    class ChessNode
-    {
-    public:
-        ChessNode(int seq)
-        {
-            std::cout << "生产奶酪：" << seq << std::endl;
-        }
-        ~ChessNode() {}
-    };
-
-public:
-    ProduceOnlineBus(uint64_t porductNum)
-    {
-        productNum_ = porductNum;
-        needTruckNum_ = productNum_ / truckCapacity_;
-    }
-    ~ProduceOnlineBus()
-    {
-    }
-
-    void start()
-    {
-        // 牛奶生产线
-        std::thread th1([&]()
-                        {
-                            int milkCount = productNum_ * milkMultiple_;
-                            for (size_t i = 0; i < milkCount; ++i)
-                            {
-                                milkNodeBlockingDeque.Put(new MilkNode(i));
-                                ++milkProded;
-                            }
-                        });
-
-        // 发酵剂生产线
-        std::thread th2([&]()
-                        {
-                            int fjjCount = productNum_ * fjjMultiple_;
-                            for (size_t i = 0; i < fjjCount; ++i)
-                            {
-                                fjjNodeBlockingDeque.Put(new FjjNode(i));
-                                ++fjjProded;
-                            }
-                        });
-
-        // 奶酪生产线
-        std::thread th3([&]()
-                        {
-                            for (size_t i = 0; i < productNum_; ++i)
-                            {
-                                MilkNode milk_node_;
-                                for (size_t j = 0; j < milkMultiple_; ++j)
-                                {
-                                    milkNodeBlockingDeque.Take(milk_node_);
-                                }
-
-                                FjjNode fjj_node_;
-                                for (size_t j = 0; j < fjjMultiple_; ++j)
-                                {
-                                    fjjNodeBlockingDeque.Take(fjj_node_);
-                                }
-
-                                cheeseNodeBlockingDeque.Put(new ChessNode(i));
-                                ++cheeseProded;
-                            }
-                        });
-
-        // 运输车辆线程
-        std::thread th4([&]()
-                        {
-                            while (trucked < needTruckNum_)
-                            {
-                                ChessNode chess_node_;
-                                for (size_t i = 0; i < truckCapacity_; ++i)
-                                {
-                                    cheeseNodeBlockingDeque.Take(chess_node_);
-                                }
-                                ++trucked;
-                            }
-                        });
-
-        if (th1.joinable())
-            th1.join();
-        if (th2.joinable())
-            th2.join();
-        if (th3.joinable())
-            th3.join();
-        if (th4.joinable())
-            th4.join();
-
-        std::cout << "生产牛奶的数量：" << milkProded << " .生产发酵剂的数量：" << fjjProded << " .生产奶酪的数量：" << cheeseProded << " .车辆运输的次数：" << trucked << std::endl;
-    }
+    SyncQueue<int> *milkNodeBlockingDeque;   // 保存牛奶的队列
+    SyncQueue<int> *fjjNodeBlockingDeque;    // 保存发酵剂的队列
+    SyncQueue<int> *cheeseNodeBlockingDeque; // 保存奶酪的队列
 };
+
+void ProduceOnlineBus::start()
+{
+    // 牛奶生产线
+    std::thread th1([&]()
+                    {
+                        for (size_t i = 0; i < milkCount_; ++i)
+                        {
+                            milkNodeBlockingDeque->Put(i);
+                            ++milkProded;
+                        }
+                    });
+
+    // 发酵剂生产线
+    std::thread th2([&]()
+                    {
+                        for (size_t i = 0; i < fjjCount_; ++i)
+                        {
+                            fjjNodeBlockingDeque->Put(i);
+                            ++fjjProded;
+                        }
+                    });
+
+    // 奶酪生产线
+    std::thread th3([&]()
+                    {
+                        for (size_t i = 0; i < productNum_; ++i)
+                        {
+                            int x;
+                            for (size_t j = 0; j < milkMultiple_; ++j)
+                            {
+                                milkNodeBlockingDeque->Take(x);
+                            }
+
+                            for (size_t j = 0; j < fjjMultiple_; ++j)
+                            {
+                                fjjNodeBlockingDeque->Take(x);
+                            }
+
+                            cheeseNodeBlockingDeque->Put(i);
+                            ++cheeseProded;
+                        }
+                    });
+
+    // 运输车辆线程
+    std::thread th4([&]()
+                    {
+                        while (trucked < needTruckNum_)
+                        {
+                            int x;
+                            for (size_t i = 0; i < truckCapacity_; ++i)
+                            {
+                                cheeseNodeBlockingDeque->Take(x);
+                            }
+                            ++trucked;
+                        }
+                    });
+
+    if (th1.joinable())
+        th1.join();
+    if (th2.joinable())
+        th2.join();
+    if (th3.joinable())
+        th3.join();
+    if (th4.joinable())
+        th4.join();
+
+    std::cout << "生产牛奶的数量：" << milkProded << " .生产发酵剂的数量：" << fjjProded << " .生产奶酪的数量：" << cheeseProded << " .车辆运输的次数：" << trucked << std::endl;
+}
 
 int main()
 {
-    ProduceOnlineBus pb = new ProduceOnlineBus(100000);
-    pb.start();
+    ProduceOnlineBus *pb = new ProduceOnlineBus(100000);
+    pb->start();
     return 0;
 }
